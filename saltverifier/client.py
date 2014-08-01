ccaa42b2245185afe236b6df02db6813f26c7bd0
Reuven V. Gonzales <reuven@virtru.com>
2014-07-31 17:16:08 -0700

    Now allow the requests to timeout and throw errors

--- saltverifier/client.py
+++ saltverifier/client.py
@@ -4,41 +4,81 @@ Client for the salt verifier server
 import zmq
 import M2Crypto
 import hashlib
+import uuid
+
+DEFAULT_CONNECTION_TIMEOUT = 10000
+DEFAULT_REQUEST_TIMEOUT = 10000
+
+
+class RequestTimeoutError(Exception):
+    pass
+
+
+def make_zmq_request(socket, message, request_timeout=DEFAULT_REQUEST_TIMEOUT,
+                     connection_timeout=DEFAULT_CONNECTION_TIMEOUT):
+    poller = zmq.Poller()
+    poller.register(socket, zmq.POLLIN | zmq.POLLOUT)
+
+    # check is the socket is ready to send. i.e. are we connected
+    poll_results = dict(poller.poll(timeout=connection_timeout))
+
+    if not poll_results.get(socket, None) == zmq.POLLOUT:
+        raise RequestTimeoutError('Connection timed out')
+
+    # Make the request
+    socket.send(message)
+
+    poll_results = dict(poller.poll(timeout=request_timeout))
+
+    if not poll_results.get(socket, None) == zmq.POLLIN:
+        raise RequestTimeoutError('Request timed out')
+
+    return socket.recv()
 
 
 def is_valid_server_public_key(address, public_key_str):
     context = zmq.Context()
+    try:
+        result = _is_valid_server_public_key(context, address, public_key_str)
+    finally:
+        context.destroy(linger=0)
+    return result
+
+
+def _is_valid_server_public_key(context, address, public_key_str):
     socket = context.socket(zmq.REQ)
 
     public_key_bio = M2Crypto.BIO.MemoryBuffer(public_key_str)
     public_key = M2Crypto.RSA.load_pub_key_bio(public_key_bio)
 
-    challenge_message = 'abc'
+    # Generate a random uuid as a challenge message
+    challenge_message = uuid.uuid4().get_bytes()
 
     encrypted_challenge_message = public_key.public_encrypt(
         challenge_message,
         M2Crypto.RSA.pkcs1_oaep_padding
     )
 
+    # Make the request to the server
     socket.connect(address)
+    encrypted_challenge_message_base64 = encrypted_challenge_message.encode('base64')
 
-    socket.send(encrypted_challenge_message.encode('base64'))
+    # Wait for a response
+    response = make_zmq_request(socket, encrypted_challenge_message_base64)
 
-    response = socket.recv()
-
+    # All responses are in the form status;message so split on ';'
     response_split = response.split(';')
 
     if response_split[0] != '200':
         return False
 
+    # Get the signature from the response
     challenge_signature_base64 = response_split[1]
-
     challenge_signature = challenge_signature_base64.decode('base64')
 
+    # Check the signature
     challenge_digest = hashlib.sha256()
-
     challenge_digest.update(challenge_message)
-
     verification_result = public_key.verify_rsassa_pss(
         challenge_digest.digest(),
         challenge_signature
@@ -46,5 +86,5 @@ def is_valid_server_public_key(address, public_key_str):
 
     if verification_result != 1:
         return False
-
+    # If we've arrived here then the signature verified correctly
     return True
